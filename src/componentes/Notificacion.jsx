3f58193a7d2160f70d527c6e4358d6272cf3935e
Santiago <santicomercio7@gmail.com>
2024-09-13 19:04:12 -0300

    Cambio de animación en el componente Notificacion

--- src/componentes/Notificacion.jsx
+++ src/componentes/Notificacion.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const Notificacion = ({mensaje}) => {
     return (
@@ -17,6 +17,28 @@ const Contenedor = styled.section`
     justify-content: center;
 `;
 
+const notify = keyframes`
+    0% {
+        transform: translateY(-1.25rem);
+        opacity: 0;
+    }
+
+    10% {
+        transform: translateY(0);
+        opacity: 1;
+    }
+
+    90% {
+        transform: translateY(0);
+        opacity: 1;
+    }
+
+    100% {
+        transform: translateY(-1.25rem);
+        opacity: 0;
+    }       
+`; 
+
 const Mensaje = styled.p`
     background-color: #04d130;
     width: 30%;
@@ -24,11 +46,13 @@ const Mensaje = styled.p`
     color: #fff;
     padding: 6px 12px;
     border-radius: 4px;
+    animation: ${notify} 3s ease forwards;
+    position: fixed;
 
     @media (max-width: 900px) {
         font-size: 14px;
         width: 60%;
     }
 `;
- 
+
 export default Notificacion;
